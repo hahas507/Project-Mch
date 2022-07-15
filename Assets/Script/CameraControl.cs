bb4ea2357e2dcb27dae0c808f955dcbb804a9149
hahas507 <80966445+hahas507@users.noreply.github.com>
2022-07-16 01:29:37 +0900

    플레이어를 항상 화면에 담기
    clamp에 대충 값을 넣었지만 나중에 화면비나 기타 값에 따라 수정될 수 있게 바꿔야함.

--- Assets/Script/CameraControl.cs
+++ Assets/Script/CameraControl.cs
@@ -20,6 +20,9 @@ public class CameraControl : MonoBehaviour
         float yRotation = yInput * camSpeed;
         currentXRotation += xRotation;
         currentYRotation -= yRotation;
+        //임시로 정했지만 추후 수정해야함.
+        currentXRotation = Mathf.Clamp(currentXRotation, -30f, 30f);
+        currentYRotation = Mathf.Clamp(currentYRotation, -11f, 30f);
         transform.localRotation = Quaternion.Euler(currentYRotation, currentXRotation, 0f);
     }
 }
