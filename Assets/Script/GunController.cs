465931838838e34a79171110afe942a9f5fdd8b7
hahas507 <80966445+hahas507@users.noreply.github.com>
2022-07-16 20:30:58 +0900

    총 조준
    Screen to ray를 사용해 플레이어가 쏘는 방향을 결정하게 만듬.

--- Assets/Script/GunController.cs
+++ Assets/Script/GunController.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class GunController : MonoBehaviour
 {
-   
+   [SerializeField] GameObject playerCam;
+    CameraControl camControl;
+
+    void Start()
+    {
+        camControl = playerCam.GetComponent<CameraControl>();
+    }
+    void Update()
+    {
+       GunAim();
+    }
+
+    void GunAim()
+    {
+       transform.LookAt(camControl.Hitpoint);       
+       Debug.DrawRay(transform.position, transform.forward * Vector3.Distance(transform.position, camControl.Hitpoint), Color.red);
+    }
 }
--- Assets/Script/TestGun.cs
+++ Assets/Script/TestGun.cs
@@ -4,19 +4,7 @@ using UnityEngine;
 
 public class TestGun : GunController
 {
-    CameraControl cameraControl;
-
     void Start()
     {
-        cameraControl = GameObject.Find("Camera").GetComponent<CameraControl>();
-    }
-    void Update()
-    {
-        GunAim();
-    }
-
-    void GunAim()
-    {
-        // Player에서 전부 컨트롤 하는게 나을듯
     }
 }
