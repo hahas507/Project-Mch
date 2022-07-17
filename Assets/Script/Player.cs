bb52c302db6930b419a78beabdc35c7934cfe7c1
hahas507 <80966445+hahas507@users.noreply.github.com>
2022-07-18 02:27:17 +0900

    회전 수정
    기존에는 rigidbody를 썼지만 수정된건 transform을 사용함
    맘에들지 않음

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float speed = 10f;
-    [Range(0, 1)] [SerializeField] float rotationSpeed;
+    [Range(0, 100)] [SerializeField] float rotationSpeedXY;
+    [Range(0, 100)] [SerializeField] float rotationSpeedZ;
     [Range(1, 5)] [SerializeField] float maxRotationSpeed;
     [SerializeField] float postureControl;
     [SerializeField] GameObject body;
@@ -55,17 +56,28 @@ public class Player : MonoBehaviour
         float curMousePosX = Mathf.InverseLerp(0, screenSize.x, mousePosition.x);
         float curMousePosY = Mathf.InverseLerp(0, screenSize.y, mousePosition.y);
         Vector3 curMousePos = new Vector3(-curMousePosY, curMousePosX, 0f);
-        Vector3 direction = ((curMousePos - (new Vector3(-1f, 1f, 0f) * 0.5f)) * 2f);
-        
-        if(Mathf.Abs(direction.x) < 0.5f)
-        {
+        Vector3 direction = ((curMousePos - (new Vector3(-1f, 1f, 0f) * 0.5f)) * 20f);
+        Debug.Log(direction);
+        if(Mathf.Abs(direction.x) < 3f)
+        {            
             direction.x = 0f;
         }            
-        if(Mathf.Abs(direction.y) < 0.5f)
+        if(Mathf.Abs(direction.y) < 3f)
         {
             direction.y = 0f;
         }
-        rb.AddRelativeTorque(direction * rotationSpeed, ForceMode.VelocityChange);
+        transform.Rotate(direction * rotationSpeedXY * Time.deltaTime);
+        
+        if(Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(Vector3.forward * rotationSpeedZ * Time.deltaTime);
+        }
+        else if(Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(Vector3.back * rotationSpeedZ * Time.deltaTime);
+        }
+        /* 
+        rb.AddRelativeTorque(direction * rotationSpeedXY, ForceMode.VelocityChange);
         if(rb.angularVelocity.magnitude > 1f)
         {
             //clamp rotation speed
@@ -76,12 +88,16 @@ public class Player : MonoBehaviour
         if(Mathf.Abs(direction.x) < 0.5f && Mathf.Abs(direction.y) < 0.5f)
         {
             rb.angularVelocity -= new Vector3(rb.angularVelocity.x, rb.angularVelocity.y, 0f) * postureControl * Time.deltaTime;
-        }
+            if(rb.angularVelocity.magnitude < 0.1f)
+            {
+                rb.angularVelocity = Vector3.zero;
+            }
+        } */
         
         //기존 코드
         /* if(direction.magnitude > 0.5f)
         {
-            rb.AddRelativeTorque(direction * rotationSpeed, ForceMode.VelocityChange);
+            rb.AddRelativeTorque(direction * rotationSpeedXY, ForceMode.VelocityChange);
             if(rb.angularVelocity.magnitude > 1f)
             {
                 //clamp rotation speed
@@ -96,18 +112,18 @@ public class Player : MonoBehaviour
         } */
 
         //Rotation control
-        if(Input.GetKey(KeyCode.Q))
+        /* if(Input.GetKey(KeyCode.Q))
         {
-            rb.AddRelativeTorque(Vector3.forward * rotationSpeed, ForceMode.VelocityChange);
+            rb.AddRelativeTorque(Vector3.forward * rotationSpeedXY, ForceMode.VelocityChange);
         }
         else if(Input.GetKey(KeyCode.E))
         {
-            rb.AddRelativeTorque(-Vector3.forward * rotationSpeed, ForceMode.VelocityChange);
+            rb.AddRelativeTorque(-Vector3.forward * rotationSpeedXY, ForceMode.VelocityChange);
         }
         else
         {
             rb.angularVelocity -= new Vector3(0f, 0f, rb.angularVelocity.z) * postureControl * Time.deltaTime;
-        }
+        } */
     }    
     
     void MouseMinMax()
@@ -118,9 +134,7 @@ public class Player : MonoBehaviour
 
     void CenterOfMass()
     {
-        //수정 필요
-        Vector3 com = transform.TransformPoint(body.transform.position);
-        rb.centerOfMass = com;
+        rb.centerOfMass = Vector3.zero;
     }
 
     //draw gizmos sphere
