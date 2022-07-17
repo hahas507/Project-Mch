02cfa6575aab3a9f378d4fdbacd2bc27bdb7839e
hahas507 <80966445+hahas507@users.noreply.github.com>
2022-07-17 15:48:21 +0900

    플레이어 회전 수정
    너무 미세하게 컨트롤 해야하는것을 방지함. 다만 addrelativeTorque가 생각하는데로 움직이지 않아 수정이 필요함

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float speed = 10f;
     [Range(0, 1)] [SerializeField] float rotationSpeed;
+    [Range(1, 5)] [SerializeField] float maxRotationSpeed;
     [SerializeField] float postureControl;
     Vector3 screenSize;
     FlightUnit flightUnit;
@@ -34,7 +35,6 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        PostureControl();
     }
 
     void FixedUpdate()
@@ -49,41 +49,65 @@ public class Player : MonoBehaviour
         {
             rb.AddForce(flightUnit.FlightUnitDir * speed);
         }
-        
-        /* //Direction control with keyboard
-        float yInput = Input.GetAxis("Horizontal");
-        float xInput = Input.GetAxis("Vertical");
-        Vector3 direction = new Vector3(-xInput, yInput, 0f);
-        rb.AddRelativeTorque(direction * rotationSpeed, ForceMode.Acceleration); */
-        
-        //mouse position
         Vector3 mousePosition = Input.mousePosition;
         float curMousePosX = Mathf.InverseLerp(0, screenSize.x, mousePosition.x);
         float curMousePosY = Mathf.InverseLerp(0, screenSize.y, mousePosition.y);
         Vector3 curMousePos = new Vector3(-curMousePosY, curMousePosX, 0f);
-        Vector3 direction = curMousePos - (new Vector3(-1f, 1f, 0f) * 0.5f);
-        rb.AddRelativeTorque(direction * rotationSpeed, ForceMode.Acceleration);
+        Vector3 direction = ((curMousePos - (new Vector3(-1f, 1f, 0f) * 0.5f)) * 2f);
+        
+        if(Mathf.Abs(direction.x) < 0.5f)
+        {
+            direction.x = 0f;
+        }            
+        if(Mathf.Abs(direction.y) < 0.5f)
+        {
+            direction.y = 0f;
+        }
+        rb.AddRelativeTorque(direction * rotationSpeed, ForceMode.VelocityChange);
+        if(rb.angularVelocity.magnitude > 1f)
+        {
+            //clamp rotation speed
+            rb.angularVelocity = new Vector3(Mathf.Clamp(rb.angularVelocity.x, -maxRotationSpeed, maxRotationSpeed),
+                                            Mathf.Clamp(rb.angularVelocity.y, -maxRotationSpeed, maxRotationSpeed),
+                                            Mathf.Clamp(rb.angularVelocity.z, -maxRotationSpeed, maxRotationSpeed));
+        }
+        if(Mathf.Abs(direction.x) < 0.5f && Mathf.Abs(direction.y) < 0.5f)
+        {
+            rb.angularVelocity -= new Vector3(rb.angularVelocity.x, rb.angularVelocity.y, 0f) * postureControl * Time.deltaTime;
+        }
+        
+        //기존 코드
+        /* if(direction.magnitude > 0.5f)
+        {
+            rb.AddRelativeTorque(direction * rotationSpeed, ForceMode.VelocityChange);
+            if(rb.angularVelocity.magnitude > 1f)
+            {
+                //clamp rotation speed
+                rb.angularVelocity = new Vector3(Mathf.Clamp(rb.angularVelocity.x, -maxRotationSpeed, maxRotationSpeed),
+                                                 Mathf.Clamp(rb.angularVelocity.y, -maxRotationSpeed, maxRotationSpeed),
+                                                 Mathf.Clamp(rb.angularVelocity.z, -maxRotationSpeed, maxRotationSpeed));
+            }
+        }
+        else
+        {
+            rb.angularVelocity -= new Vector3(rb.angularVelocity.x, rb.angularVelocity.y, 0f) * postureControl * Time.deltaTime;
+        } */
 
         //Rotation control
         if(Input.GetKey(KeyCode.Q))
         {
-            rb.AddRelativeTorque(Vector3.forward * rotationSpeed, ForceMode.Acceleration);
+            rb.AddRelativeTorque(Vector3.forward * rotationSpeed, ForceMode.VelocityChange);
         }
-        if(Input.GetKey(KeyCode.E))
+        else if(Input.GetKey(KeyCode.E))
         {
-            rb.AddRelativeTorque(-Vector3.forward * rotationSpeed, ForceMode.Acceleration);
+            rb.AddRelativeTorque(-Vector3.forward * rotationSpeed, ForceMode.VelocityChange);
+        }
+        else
+        {
+            rb.angularVelocity -= new Vector3(0f, 0f, rb.angularVelocity.z) * postureControl * Time.deltaTime;
         }
     }    
     
-    void PostureControl()
-    {
-        //Posture control
-        if(Input.GetKey(KeyCode.F))
-        {
-            rb.angularVelocity -= rb.angularVelocity * postureControl * Time.deltaTime;
-        }
-    }
-
     void MouseMinMax()
     {
         screenSize.x = Screen.width;
